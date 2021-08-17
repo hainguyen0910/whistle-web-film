62f802cc57851af0772f3fa09bf9ecb113a99529
hainguyen0910 <nguyencn17a@gmail.com>
2021-08-17 10:58:11 +0700

    :recycle: refactor footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,15 +2,10 @@ import {
   Box,
   Container,
   Divider,
-  Flex,
   Link,
-  List,
-  ListIcon,
-  ListItem,
   SimpleGrid,
   Stack,
   Text,
-  useColorModeValue,
 } from '@chakra-ui/react';
 import React from 'react';
 import {
@@ -21,7 +16,6 @@ import {
   FaTwitter,
   FaYoutube,
 } from 'react-icons/fa';
-import { MdCheckCircle } from 'react-icons/md';
 import AppStoreBadge from './AppStoreBadge';
 import CHPlayStoreBadge from './CHPlayStoreBadge';
 import SocialButton from './SocialButton';
@@ -36,81 +30,7 @@ function ListHeader({ children }) {
 
 function Footer() {
   return (
-    // <Flex
-    //   p="4.375em 2.5em"
-    //   m="0 auto"
-    //   color="gray.500"
-    //   justify="center"
-    //   alignItems="center"
-    // >
-    //   <Box maxW="1000px">
-    //     <Link href="/">Questions? Contact us.</Link>
-    //     <Flex alignItems="center">
-    //       <List spacing={5} mr={10}>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">FAQ</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Help Center</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Account</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Media Center</Link>
-    //         </ListItem>
-    //       </List>
-
-    //       <List spacing={5} mr={10}>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Investor Relations</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Jobs</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Privacy</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Speed Test</Link>
-    //         </ListItem>
-    //       </List>
-
-    //       <List spacing={5}>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Cookie Preferences</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Corporate Information</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Contact Us</Link>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListIcon as={MdCheckCircle} color="green.500" />
-    //           <Link href="/">Contact Us</Link>
-    //         </ListItem>
-    //       </List>
-    //     </Flex>
-    //     <Divider mt={10} />
-    //   </Box>
-    // </Flex>
-    <Box
-      // bg={useColorModeValue('gray.50', 'gray.900')}
-      // color={useColorModeValue('gray.700', 'gray.200')}
-      color="gray.500"
-    >
+    <Box color="gray.500">
       <Container as={Stack} maxW="6xl" py={10}>
         <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
           <Stack align="flex-start">
